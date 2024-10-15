cd68d5867a2a10a5e082075c4e25e340995842c6
tbaumberger <tbaumberger@student.ethz.ch>
2024-10-15 23:47:34 +0200

    Add feedback link

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ const total = endOfSemester - startOfSemester;
 const current = now - startOfSemester;
 const percentage = Math.min(100, Math.ceil((current / total) * 100));
 progressBar.style.width = percentage + "%";
+
+/*
+const regex = /((?:[\s\S]*\s+)?for\s*\(([^;]*)\s*;\s*([^;]*)\s*;\s*(.*)\s*\)\s*{[\s\S]*}[\s\S]*)/;
+
+const groups = "for (int i = 0; i < 10; i++) {}".match(regex);
+console.log("Gruppe 2: " + groups[2].trim() + " FIN");
+console.log(groups[3].trim());
+console.log(groups[4].trim());
+
+
+
+*/
